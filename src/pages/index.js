b02fa0c8f4775779b5a70ebd3391724743f016cf
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-03-13 22:29:24 +0700

    💻 Coming Soon Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import logo from "../images/logo.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Under Construction" />
-    <h1 className="text-4xl">this site is under construction</h1>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <img src={logo} className="h-40 mx-auto" />
+        <h1 className="text-2xl mt-4 text-center">coming soon</h1>
+      </div>
+    </div>
   </Layout>
 )
 
